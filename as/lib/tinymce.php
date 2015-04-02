6b1ba87e72761f782ac1c9955cd2ac1dd860c5f7
Hiler Liu <steelcal@gmail.com>
2015-04-02 11:28:34 +0800

    1.5.0

--- as/lib/tinymce.php
+++ as/lib/tinymce.php
@@ -42,14 +42,31 @@ function editorEnable(id){
 	convert_urls : false ,
 	elements : id,
 	plugins: [
-					'advlist autolink autosave link image lists charmap preview hr anchor autoresize',
+					'advlist autolink autosave link image lists charmap preview hr anchor',
 					'searchreplace code fullscreen insertdatetime media',
-					'table contextmenu directionality textcolor'
+					'table contextmenu directionality textcolor pagebreak'
 	],
 
-	toolbar1: 'undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | formatselect fontselect fontsizeselect | table',
+	toolbar1: 'undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | formatselect fontselect fontsizeselect | table | pagebreak',
 	toolbar2: 'bullist numlist | searchreplace | outdent indent rtl blockquote | link unlink anchor | image media inserttime hr | removeformat forecolor backcolor | code preview fullscreen',
-
+   font_formats: "Andale Mono=andale mono,times;"+
+        "Arial=arial,helvetica,sans-serif;"+
+        "Arial Black=arial black,avant garde;"+
+        "Book Antiqua=book antiqua,palatino;"+
+        "Comic Sans MS=comic sans ms,sans-serif;"+
+        "Courier New=courier new,courier;"+
+        "Georgia=georgia,palatino;"+
+        "Helvetica=helvetica;"+
+        "Impact=impact,chicago;"+
+        "Microsoft YaHei=Microsoft YaHei;"+
+        "Symbol=symbol;"+
+        "Tahoma=tahoma,arial,helvetica,sans-serif;"+
+        "Terminal=terminal,monaco;"+
+        "Times New Roman=times new roman,times;"+
+        "Trebuchet MS=trebuchet ms,geneva;"+
+        "Verdana=verdana,geneva;"+
+        "Webdings=webdings;"+
+        "Wingdings=wingdings,zapf dingbats",
 
 	menubar: false,
 <?php
