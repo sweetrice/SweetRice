3c2152b1e94f09a7a43aa7445a018c1df1891489
zhishix.com <dev@zhishix.com>
2021-05-17 12:18:04 +0800

    init 1.7.0

--- inc/do_lang.php
+++ inc/do_lang.php
@@ -10,7 +10,7 @@
 	$lang = $_POST['lang'];
 	if(file_exists(LANG_DIR.$lang.'.php')){
 		setcookie('lang',$lang,time() + 31536000,BASE_DIR);
-		setcookie('lang_update',time(),BASE_DIR);
+		setcookie('lang_update',time(),0,BASE_DIR);
 		output_json(array('status'=>1));
 	}else{
 		output_json(array('status'=>0));
